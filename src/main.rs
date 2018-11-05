7dde23a5dc86fe3f2f47a533d5495367c06811e0
Guilhem Radonde <Guilhem.Radonde@itesoft.com>
2018-11-05 14:03:48 +0100

    add the possibility to pass a custom config file by arguments

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,51 @@ extern crate serde_json;
 
 use configuration::Configuration;
 use configuration::Job;
+use std::env;
 
 mod fs;
 mod api;
 mod configuration;
 
-const PROPERTIES_FILE: &str = "./config.json";
 
 fn main() {
-    match std::fs::read_to_string(PROPERTIES_FILE){
+
+    let args: Vec<_> = env::args().collect();
+    let mut properties_file: &str = "./config.json";
+    
+    if args.len() > 1 {
+        println!("The first argument is {}", args[1]);
+        properties_file = args[1].as_str();
+    }
+
+    match std::fs::read_to_string(properties_file){
         Ok(config)=>{
             match serde_json::from_str(&config){
                 Ok::<Configuration,serde_json::Error>(config)=>{
-                
+
                     println!("Configuration : {:#?}",config);
-                    
+
                     for job in &config.jobs {
-                        treat_job(job);                                 
+                        treat_job(job);
                     }
 
                     println!("All done, that's all folks...");
 
                 },
                 Err(e) =>{
-                    eprintln!("An error occured while deserializing config from file {} : {}", PROPERTIES_FILE, e);
+                    eprintln!("An error occured while deserializing config from file {} : {}", properties_file, e);
                 },
             };
         },
         Err(e)=>{
-            eprintln!("An error occured while opening file {} : {}", PROPERTIES_FILE, e);
+            eprintln!("An error occured while opening file {} : {}", properties_file, e);
         },
     };
 }
 
 
 
-fn treat_job(job: &Job) {        
+fn treat_job(job: &Job) {
     match job {
         Job::Copy(job_copy) => {
             println!("Will treat {}", job_copy.name);
@@ -63,7 +72,7 @@ fn treat_job(job: &Job) {
                 },
             };
         },
-    };                                                   
+    };
 }
 
 #[cfg(test)]
@@ -72,7 +81,7 @@ mod tests {
     use std::path::Path;
     use std::fs;
     use std::io;
-    
+
     const TEST_DIR: &str = "./test/";
     const TEST_DIR_1: &str = "./test/test_dir_1/";
     const TEST_DIR_2: &str = "./test/test_dir_2/";
@@ -83,28 +92,27 @@ mod tests {
     #[test]
     fn test_with_basic_config() {
         let result_setup = setup();
-        assert!(result_setup.is_ok()); 
-        
+        assert!(result_setup.is_ok(), "##### {:#?}", result_setup);
+
         main();
         assert!(Path::new(TEST_DIR_RESULT).exists());
         assert!(Path::new(TEST_FILE_RESULT).exists());
-        
+
         let result_clean = clean();
-        assert!(result_clean.is_ok());    
+        assert!(result_clean.is_ok());
     }
-    
+
     fn setup() -> Result<(), io::Error>{
         fs::create_dir_all(TEST_DIR_1)?;
         fs::OpenOptions::new().create(true).write(true).open(TEST_FILE)?;
         fs::create_dir_all(TEST_DIR_2)?;
-        
+
         Ok(())
     }
-    
-    fn clean() -> Result<(), io::Error>{  
+
+    fn clean() -> Result<(), io::Error>{
         fs::remove_dir_all(TEST_DIR)?;
         Ok(())
     }
-   
-}
 
+}
